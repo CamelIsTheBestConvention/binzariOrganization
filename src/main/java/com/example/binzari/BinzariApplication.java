fb7c8fab0c6b03ec78e57e282907aa3d155fef8f
dev-AustinKim <ehdgks0412@gmail.com>
2023-06-20 17:40:56 +0900

    김동한 환경설정

---
+++ src/main/java/com/example/binzari/BinzariApplication.java
@@ -0,0 +1,13 @@
+package com.example.binzari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinzariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinzariApplication.class, args);
+    }
+
+}
