cd71b253995270f6a18e8e6a7a711205cc5409a4
CamelIsTheBestConvention <ansdj3523@gmail.com>
2023-07-06 18:51:52 +0900

    modifying myPage divide normal and master

--- src/main/frontend/src/components/myPageContent/myProfile.js
+++ src/main/frontend/src/components/myPageContent/myProfile.js
@@ -1,13 +1,22 @@
 import styled from "styled-components";
-import ProfileImg from "./profileImg";
-import ProfileInfo from "./profileInfo";
+import ProfileFace from "../../images/myPageImages/personProfile.png";
+import ProfileArrowImg from "../../images/myPageImages/arrow.jpg";
+import React, { useState } from "react";
 
 const MyProfile = () => {
   return (
     <>
       <MyProfileWrapper>
-        <ProfileImg />
-        <ProfileInfo />
+        <ProfileImgWrapper>
+          <ProfileImgImg src={ProfileFace} />
+        </ProfileImgWrapper>
+        <ProfileInfoWrapper>
+          <ProfileNickName>
+            문성준
+            <ArrowImg src={ProfileArrowImg} />
+          </ProfileNickName>
+          <ProfileType>11</ProfileType>
+        </ProfileInfoWrapper>
       </MyProfileWrapper>
     </>
   );
@@ -17,4 +26,37 @@ export default MyProfile;
 const MyProfileWrapper = styled.div`
   display: flex;
   margin-bottom: 0.3em;
+`;
+
+const ProfileImgWrapper = styled.div`
+  border-radius: 70%;
+  max-width: 55px;
+  max-height: 55px;
+  overflow: hidden;
+`;
+
+const ProfileImgImg = styled.img`
+  height: 100%;
+  object-fit: cover;
+`;
+
+const ProfileInfoWrapper = styled.div`
+  display: inline-block;
+  margin: auto 0 auto 1rem;
+`;
+
+const ProfileNickName = styled.p`
+  margin: 3px 0px;
+  font-weight: bold;
+  font-size: 1.3rem;
+`;
+
+const ArrowImg = styled.img`
+  height: 18px;
+  margin-left: 0.3rem;
+`;
+
+const ProfileType = styled.p`
+  margin: 3px 0px;
+  font-size: 0.8rem;
 `;
